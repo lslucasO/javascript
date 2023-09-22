0ad08a14337d3807322690d87eae20ddb0ce7e44
Lucas Santana Lemos <lslemos59@gmail.com>
2023-09-22 10:40:26 -0300

    #

--- ex007.js
+++ ex007.js
@@ -1,5 +1,9 @@
 let idade = 31;
 let nome = "Lucas";
 let salario = 1050.40;
+let estaChovendo = true;
+
 
 console.log(typeof(idade), typeof(nome), typeof(salario));
+
+console.log("Está chovendo? " + estaChovendo);
