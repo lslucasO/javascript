901ed2ee9e6a1f6b96259cb568c0a127a84bfec0
Lucas Santana Lemos <lslemos59@gmail.com>
2023-09-22 10:39:06 -0300

    #

---
+++ ex007.js
@@ -0,0 +1,5 @@
+let idade = 31;
+let nome = "Lucas";
+let salario = 1050.40;
+
+console.log(typeof(idade), typeof(nome), typeof(salario));
---
+++ modulo01/ex006.js
@@ -0,0 +1,5 @@
+let preco = 20;
+let desconto = 5.40;
+let precoDesconto = (preco - desconto);
+
+console.log("O preço final foi: R$" + precoDesconto);
